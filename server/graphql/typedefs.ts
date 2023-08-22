aeded286d98b807abbc85def4a70e72b663b6e1a
babucarr32 <baboucarry@gmai.com>
2023-08-22 22:01:00 +0000

    Created some typedefs in the typedefs in graphql folder

---
+++ server/graphql/typedefs.ts
@@ -0,0 +1,37 @@
+export const typeDefs = `#graphql
+type User{
+    username: String!
+    fullName: String!
+    email: String!
+    password: String!
+    followers: [Follow]
+    profileImage: String
+    coverImage: String
+}
+
+type Post{
+    title: String!
+    description:String!
+    images: [String]
+    likes: [Like]
+    dislikes: [Like]
+    comments: [Comment]
+}
+
+type Follow{
+    user_id: String!
+}
+
+type Comment{
+    comment: String!
+    user_id: ID!
+    post_id: ID!
+    likes: Number
+    dislikes: Number
+}
+
+type Like{
+    user_id: ID!
+    post_id: ID!
+}
+`;
